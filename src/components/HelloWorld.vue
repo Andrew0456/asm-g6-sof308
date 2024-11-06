8e159db13194d404bf50677cd22277f930320e92
Andrew0456 <phandat452006@gmail.com>
2024-11-07 02:13:58 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ greeting }}</h1>
+  <button :title="buttonTitle" @click="changeTitle">Click để thay đổi lời chào</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Xin chào bạn!',
+      buttonTitle: 'Nhấn để thay đổi lời chào'
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.greeting = 'Hello, World!';
+      this.buttonTitle = 'Lời chào mới đã được cập nhật';
+    }
+  }
+}
+</script>
